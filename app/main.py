b5c46a1ab48d2991bea712411b9deb77c15146b0
David Henrique <dh210605@gmail.com>
2025-02-05 00:25:57 -0300

    resolved

--- app/main.py
+++ app/main.py
@@ -18,11 +18,11 @@ class Usuario(BaseModel):
     nome: str
     email: str
 
-@app.get("/usuarios/")
-async def listar_usuarios():
-    return {"message": "API funcionando!"}
+@app.get("/")
+async def home():
+    return {"message": "API rodando na Render 🚀"}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  
-    print(f"Iniciando servidor na porta {port}")  
+    port = int(os.getenv("PORT", 10000)) 
+    print(f"Iniciando servidor na porta {port}...")  
     uvicorn.run(app, host="0.0.0.0", port=port)
